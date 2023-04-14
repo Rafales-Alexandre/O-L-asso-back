90d8afaa243605ac803dbdf4681884876de2970f
AnaelleIghil <anaelle.ighil@gmail.com>
2023-04-14 16:49:11 +0200

    apollo server on, issue with querie One element and resolvers

--- app/resolvers/user.js
+++ app/resolvers/user.js
@@ -2,12 +2,12 @@ const instrumentDatamapper = require ("../datamappers/instrument");
 const userHasSuitDatamapper = require ("../datamappers/userHasSuit");
 
 const resolvers = {
-instrument (parent){
-    return instrumentDatamapper.findByPk(parent.user_id);
+instrument(parent){
+    return instrumentDatamapper.findByPk(parent.instrument_id);
 },
 
 userHasSuit(parent){
-    return userHasSuitDatamapper.findByPk(parent.suit_id)
+    return userHasSuitDatamapper.findByPk(parent.userHasSuit_id)
 }
 
 }
