7a7f895632b47d47ab7993d60619a19393324840
Jean-Charles-Audebert <jc1932@gmail.com>
2023-04-23 09:28:40 +0200

    Revert "Co-authored-by: Alvan <Alvan-Judi@users.noreply.github.com>"
    This reverts commit f9da6e61de93a50ef432764a47a877252aea926b.

---
+++ server.js
@@ -0,0 +1,75 @@
+/**
+ * Batala Main
+ * Main requiement to work has a ApolloServer/Express API
+ */
+require("dotenv").config();
+const getTokenForRequest = require("./app/services/getTokenForRequest");
+const express = require("express");
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
+//const { ApolloServerPluginDrainHttpServer } = require('@apollo/server/plugin/drainHttpServer');
+const { json } = require ("body-parser")
+const cors = require('cors');
+const jwt = require("jsonwebtoken");
+
+/**
+ * Non official features yet:
+const { ApolloServerPluginDrainHttpServer} = require('@apollo/server/plugin/drainHttpServer')
+
+ */
+
+/**
+ * Context of Appollo : 
+ */
+const {app,apolloConfig} = require("./app");
+
+const http = require("http");
+const serverHTTP = http.createServer(app);
+
+const PORT = process.env.PORT ?? 3003;
+
+
+const server = new ApolloServer(apolloConfig);
+
+/**
+ * Deployment 
+ * 
+ */
+(async ()=>{
+	await server.start();
+	app.use("/graphql",cors(),json(),expressMiddleware(server, {
+		context : async ({ req, res }) => {
+			const token = await getTokenForRequest(req);
+
+			const data = jwt.verify(token, 'olasso');
+			if(data.user) {
+				return {
+					user: data.user
+				};
+			} if (token) {
+console.log(req.headers)
+    try {
+      const data = jwt.verify(token, 'olasso');
+      if (data.user) {
+        return {
+          user: data.user,
+        };
+      }
+    } catch (err) {
+      console.error("JWT verification failed:", err.message);
+    }
+    return {};
+  }
+
+
+			return {};
+		}
+	}));
+
+	await new Promise ((resolve) => serverHTTP.listen (PORT, resolve));
+	console.log(`🚀 On décolle ici http://localhost:${PORT}/graphql`);
+}) ();
+
+/**
+ * how to send the header to the autorization
+ */
